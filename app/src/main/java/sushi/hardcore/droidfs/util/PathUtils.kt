7ca9398766cf98171993468cd0e29bbc1af782d8
Hardcore Sushi <hardcore.sushi@disroot.org>
2021-12-20 20:24:07 +0100

    Fix size formatting

--- app/src/main/java/sushi/hardcore/droidfs/util/PathUtils.kt
+++ app/src/main/java/sushi/hardcore/droidfs/util/PathUtils.kt
@@ -89,8 +89,8 @@ object PathUtils {
         if (size <= 0) {
             return "0 B"
         }
-        val digitGroups = (log10(size.toDouble()) / log10(1024.0)).toInt()
-        return DecimalFormat("#,##0.#").format(size / 1024.0.pow(digitGroups.toDouble())
+        val digitGroups = (log10(size.toDouble()) / log10(1000.0)).toInt()
+        return DecimalFormat("#,##0.#").format(size / 1000.0.pow(digitGroups.toDouble())
         ) + " " + units[digitGroups]
     }
 
