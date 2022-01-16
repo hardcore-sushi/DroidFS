55883172a1e411457303ac77535ebde78f6889f6
Hardcore Sushi <hardcore.sushi@disroot.org>
2022-01-16 11:54:29 +0100

    Consider WebP as image

--- app/src/main/java/sushi/hardcore/droidfs/ConstValues.kt
+++ app/src/main/java/sushi/hardcore/droidfs/ConstValues.kt
@@ -16,7 +16,7 @@ class ConstValues {
         const val wipe_passes = 2
         const val slideshow_delay: Long = 4000
         private val fileExtensions = mapOf(
-            Pair("image", listOf("png", "jpg", "jpeg", "gif", "bmp")),
+            Pair("image", listOf("png", "jpg", "jpeg", "gif", "webp", "bmp")),
             Pair("video", listOf("mp4", "webm", "mkv", "mov")),
             Pair("audio", listOf("mp3", "ogg", "m4a", "wav", "flac")),
             Pair("text", listOf("txt", "json", "conf", "log", "xml", "java", "kt", "py", "pl", "rb", "go", "c", "h", "cpp", "hpp", "rs", "sh", "bat", "js", "html", "css", "php", "yml", "yaml", "toml", "ini", "md", "properties"))
